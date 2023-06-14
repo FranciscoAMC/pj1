9f6532bd734aac675f9336a3f44965a7a10562dd
FranciscoAMC <109702404+FranciscoAMC@users.noreply.github.com>
2023-06-14 17:35:23 -0600

    Add files via upload

---
+++ Regexp.java
@@ -0,0 +1,13 @@
+// Esta es la clase principal donde debe tener su metodo main. 
+// Para este proyecto tiene la libertad de ejecutar su programa como desee
+// siempre que se cumplan los lineamientos de las instrucciones.
+public class Regexp {
+
+    // Implemente aqui su metodo main. Recuerde que debe recibir los argumentos en este orden
+    // 1. en path con el archivo .rgx donde esta la informacion de la expresion regular
+    // 2. El modo de ejecucion [-afd|-gld|-min|-eval]
+    // 3. El nombre del archivo de salida donde escribiran el resultado (para la bandera -eval, este argumento no se toma en cuenta)
+    public static void main(String[] args) throws Exception {
+
+    }
+}
